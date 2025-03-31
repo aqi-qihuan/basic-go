a0f84d3f0f7ec50f9b4285da95885420ad68ba9f
AqiSevenSeven <aqi2316364297@gmail.com>
2025-03-31 23:40:17 +0800

    LocalCodeCache 本地缓存实现

--- lmbook/internal/integration/startup/wire.go
+++ lmbook/internal/integration/startup/wire.go
@@ -3,14 +3,23 @@
 package startup
 
 import (
+	"basic-go/lmbook/internal/repository"
+	"basic-go/lmbook/internal/repository/cache"
+	"basic-go/lmbook/internal/repository/dao"
+	"basic-go/lmbook/internal/service"
+	"basic-go/lmbook/internal/service/sms"
+	"basic-go/lmbook/internal/service/sms/async"
 	"basic-go/lmbook/internal/web"
 	ijwt "basic-go/lmbook/internal/web/jwt"
+	"basic-go/lmbook/ioc"
+	"github.com/gin-gonic/gin"
 	"github.com/google/wire"
 )
 
 var thirdPartySet = wire.NewSet( // 第三方依赖
 	InitRedis, InitDB,
-					InitLogger)
+	InitLogger)
+
 var userSvcProvider = wire.NewSet( // Service 部分
 	dao.NewUserDAO,
 	cache.NewCodeCache,
@@ -21,6 +30,12 @@ var articlSvcProvider = wire.NewSet(
 	cache.NewArticleRedisCache,
 	dao.NewArticleGORMDAO,
 	service.NewArticleService)
+
+var interactiveSvcSet = wire.NewSet(dao.NewGORMInteractiveDAO,
+	cache.NewInteractiveRedisCache,
+	repository.NewCachedInteractiveRepository,
+	service.NewInteractiveService,
+)
 
 func InitWebServer() *gin.Engine {
 	wire.Build(
@@ -50,6 +65,14 @@ func InitWebServer() *gin.Engine {
 	return gin.Default()
 }
 
+func InitAsyncSmsService(svc sms.Service) *async.Service {
+	wire.Build(thirdPartySet, repository.NewAsyncSMSRepository,
+		dao.NewGORMAsyncSmsDAO,
+		async.NewService,
+	)
+	return &async.Service{}
+}
+
 func InitArticleHandler(dao dao.ArticleDAO) *web.ArticleHandler {
 	wire.Build(
 		thirdPartySet,
@@ -60,4 +83,9 @@ func InitArticleHandler(dao dao.ArticleDAO) *web.ArticleHandler {
 		web.NewArticleHandler,
 	)
 	return &web.ArticleHandler{}
+}
+
+func InitInteractiveService() service.InteractiveService {
+	wire.Build(thirdPartySet, interactiveSvcSet)
+	return service.NewInteractiveService(nil)
 }
--- lmbook/internal/repository/cache/code_local.go
+++ lmbook/internal/repository/cache/code_local.go
@@ -1,1 +1,27 @@
 package cache
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	lru "github.com/hashicorp/golang-lru"
+	"sync"
+	"time"
+)
+
+// 技术选型考虑的点
+//  1. 功能性：功能是否能够完全覆盖你的需求。
+//  2. 社区和支持度：社区是否活跃，文档是否齐全，
+//     以及百度（搜索引擎）能不能搜索到你需要的各种信息，有没有帮你踩过坑
+//  3. 非功能性：易用性（用户友好度，学习曲线要平滑），
+//     扩展性（如果开源软件的某些功能需要定制，框架是否支持定制，以及定制的难度高不高）
+//     性能（追求性能的公司，往往有能力自研）
+
+// LocalCodeCache 本地缓存实现
+type LocalCodeCache struct {
+	cache *lru.Cache
+	// 普通锁，或者说写锁
+	lock sync.Mutex
+	//读写锁
+	expiration time.Duration
+}
