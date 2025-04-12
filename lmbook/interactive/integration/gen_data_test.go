25bc2b361b56aa9ea8f582656564774777390d5d
AqiSevenSeven <aqi2316364297@gmail.com>
2025-04-12 23:55:30 +0800

    不停机数据迁移

--- lmbook/interactive/integration/gen_data_test.go
+++ lmbook/interactive/integration/gen_data_test.go
@@ -1,1 +1,90 @@
 package integration
+
+import (
+	"basic-go/lmbook/interactive/repository/dao"
+	"basic-go/lmbook/internal/integration/startup"
+	_ "embed"
+	"github.com/stretchr/testify/require"
+	"gorm.io/gorm"
+	"math/rand"
+	"os"
+	"strconv"
+	"testing"
+	"time"
+)
+
+//go:embed init.sql
+var initSQL string
+
+// TestGenSQL 这个测试就是用来生成 SQL，你在启动 Docker 的时候导入进去。
+func TestGenSQL(t *testing.T) {
+	file, err := os.OpenFile("data.sql",
+		os.O_RDWR|os.O_APPEND|os.O_CREATE|os.O_TRUNC, 0666)
+	require.NoError(t, err)
+	defer file.Close()
+
+	//创建数据库和数据表的语句，防止还没初始化
+	_, err = file.WriteString(initSQL)
+	require.NoError(t, err)
+
+	const prefix = "INSERT INTO `interactives`(`biz_id`, `biz`, `read_cnt`, `collect_cnt`, `like_cnt`, `ctime`, `utime`)\nVALUES"
+	const rowNum = 1000
+
+	now := time.Now().UnixMilli()
+	_, err = file.WriteString(prefix)
+
+	for i := 0; i < rowNum; i++ {
+		if i > 0 {
+			file.Write([]byte{',', '\n'})
+		}
+		file.Write([]byte{'('})
+		// biz_id
+		file.WriteString(strconv.Itoa(i + 1))
+		// biz
+		file.WriteString(`,"test",`)
+		// read_cnt
+		file.WriteString(strconv.Itoa(int(rand.Int31n(10000))))
+		file.Write([]byte{','})
+		// collect_cnt
+		file.WriteString(strconv.Itoa(int(rand.Int31n(10000))))
+		file.Write([]byte{','})
+		// like_cnt
+		file.WriteString(strconv.Itoa(int(rand.Int31n(10000))))
+		file.Write([]byte{','})
+		// ctime
+		file.WriteString(strconv.FormatInt(now, 10))
+		file.Write([]byte{','})
+		// utime
+		file.WriteString(strconv.FormatInt(now, 10))
+		file.Write([]byte{')'})
+	}
+}
+
+func TestGenData(t *testing.T) {
+
+	db := startup.InitDB()
+
+	for i := 0; i < 10; i++ {
+
+		const batchSize = 100
+		data := make([]*dao.Interactive, 0, batchSize)
+		now := time.Now().UnixMilli()
+		for j := 0; j < batchSize; j++ {
+			data = append(data, &dao.Interactive{
+				Biz:        "test",
+				BizId:      int64(i*batchSize + j + 1),
+				ReadCnt:    rand.Int63(),
+				LikeCnt:    rand.Int63(),
+				CollectCnt: rand.Int63(),
+				Ctime:      now,
+				Utime:      now,
+			})
+		}
+		err := db.Transaction(func(tx *gorm.DB) error {
+			err := tx.Create(data).Error
+			require.NoError(t, err)
+			return err
+		})
+		require.NoError(t, err)
+	}
+}
--- lmbook/interactive/ioc/migrator.go
+++ lmbook/interactive/ioc/migrator.go
@@ -1,1 +1,52 @@
 package ioc
+
+import (
+	"basic-go/lmbook/interactive/repository/dao"
+	"basic-go/lmbook/pkg/ginx"
+	"basic-go/lmbook/pkg/gormx/connpool"
+	"basic-go/lmbook/pkg/logger"
+	"basic-go/lmbook/pkg/migrator/events"
+	"basic-go/lmbook/pkg/migrator/events/fixer"
+	"basic-go/lmbook/pkg/migrator/scheduler"
+	"github.com/IBM/sarama"
+	"github.com/gin-gonic/gin"
+	prometheus2 "github.com/prometheus/client_golang/prometheus"
+	"github.com/spf13/viper"
+)
+
+// InitGinxServer 管理后台的 server
+func InitGinxServer(l logger.LoggerV1,
+	src SrcDB,
+	dst DstDB,
+	pool *connpool.DoubleWritePool,
+	producer events.Producer) *ginx.Server {
+	engine := gin.Default()
+	group := engine.Group("/migrator")
+	ginx.InitCounter(prometheus2.CounterOpts{
+		Namespace: "geektime_daming",
+		Subsystem: "lmbook_intr_admin",
+		Name:      "biz_code",
+		Help:      "统计业务错误码",
+	})
+	sch := scheduler.NewScheduler[dao.Interactive](l, src, dst, pool, producer)
+	sch.RegisterRoutes(group)
+	return &ginx.Server{
+		Engine: engine,
+		Addr:   viper.GetString("migrator.http.addr"),
+	}
+}
+
+func InitInteractiveProducer(p sarama.SyncProducer) events.Producer {
+	return events.NewSaramaProducer("inconsistent_interactive", p)
+}
+
+func InitFixerConsumer(client sarama.Client,
+	l logger.LoggerV1,
+	src SrcDB,
+	dst DstDB) *fixer.Consumer[dao.Interactive] {
+	res, err := fixer.NewConsumer[dao.Interactive](client, l, "inconsistent_interactive", src, dst)
+	if err != nil {
+		panic(err)
+	}
+	return res
+}
