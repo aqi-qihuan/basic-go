83005faad991c2634f8399c342b28da4920c435e
AqiSevenSeven <aqi2316364297@gmail.com>
2025-07-19 23:52:39 +0800

    follow 微服务

--- lmbook/follow/integration/startup/log.go
+++ lmbook/follow/integration/startup/log.go
@@ -1,1 +1,9 @@
 package startup
+
+import (
+	"basic-go/lmbook/pkg/logger"
+)
+
+func InitLog() logger.LoggerV1 {
+	return logger.NewNoOpLogger()
+}
--- lmbook/follow/integration/startup/wire.go
+++ lmbook/follow/integration/startup/wire.go
@@ -1,1 +1,26 @@
+//go:build wireinject
+
 package startup
+
+import (
+	"basic-go/lmbook/follow/grpc"
+	"basic-go/lmbook/follow/repository"
+	"basic-go/lmbook/follow/repository/cache"
+	"basic-go/lmbook/follow/repository/dao"
+	"basic-go/lmbook/follow/service"
+	"github.com/google/wire"
+)
+
+func InitServer() *grpc.FollowServiceServer {
+	wire.Build(
+		InitRedis,
+		InitLog,
+		InitTestDB,
+		dao.NewGORMFollowRelationDAO,
+		cache.NewRedisFollowCache,
+		repository.NewFollowRelationRepository,
+		service.NewFollowRelationService,
+		grpc.NewFollowRelationServiceServer,
+	)
+	return new(grpc.FollowServiceServer)
+}
