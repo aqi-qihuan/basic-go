cfae82457f523963ad687c8022a551a15f5dd9f2
AqiSevenSeven <aqi2316364297@gmail.com>
2025-07-20 23:50:20 +0800

    follow 微服务

--- lmbook/follow/ioc/grpc.go
+++ lmbook/follow/ioc/grpc.go
@@ -1,1 +1,25 @@
 package ioc
+
+import (
+	grpc2 "basic-go/lmbook/follow/grpc"
+	"basic-go/lmbook/pkg/grpcx"
+	"github.com/spf13/viper"
+	"google.golang.org/grpc"
+)
+
+func InitGRPCxServer(followService *grpc2.FollowServiceServer) *grpcx.Server {
+	type Config struct {
+		Addr string `yaml:"addr"`
+	}
+	var cfg Config
+	err := viper.UnmarshalKey("grpc", &cfg)
+	if err != nil {
+		panic(err)
+	}
+	server := grpc.NewServer()
+	followRelation.Register(server)
+	return &grpcx.Server{
+		Server: server,
+		Addr:   cfg.Addr,
+	}
+}
--- lmbook/follow/repository/cache/types.go
+++ lmbook/follow/repository/cache/types.go
@@ -1,1 +1,13 @@
 package cache
+
+import (
+	"basic-go/lmbook/follow/domain"
+	"context"
+)
+
+type FollowCache interface {
+	StaticsInfo(ctx context.Context, uid int64) (domain.FollowStatics, error)
+	SetStaticsInfo(ctx context.Context, uid int64, statics domain.FollowStatics) error
+	Follow(ctx context.Context, follower, followee int64) error
+	CancelFollow(ctx context.Context, follower, followee int64) error
+}
