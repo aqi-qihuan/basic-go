c85d208da9e3c9df3a64f3d018c192798ceb1273
AqiSevenSeven <aqi2316364297@gmail.com>
2025-07-21 23:37:07 +0800

    follow 微服务

--- lmbook/follow/wire.go
+++ lmbook/follow/wire.go
@@ -1,1 +1,34 @@
-package comment
+//go:build wireinject
+
+package main
+
+import (
+	grpc2 "basic-go/lmbook/follow/grpc"
+	"basic-go/lmbook/follow/ioc"
+	"basic-go/lmbook/follow/repository"
+	"basic-go/lmbook/follow/repository/dao"
+	"basic-go/lmbook/follow/service"
+	"github.com/google/wire"
+)
+
+var serviceProviderSet = wire.NewSet(
+	dao.NewGORMFollowRelationDAO,
+	repository.NewFollowRelationRepository,
+	service.NewFollowRelationService,
+	grpc2.NewFollowRelationServiceServer,
+)
+
+var thirdProvider = wire.NewSet(
+	ioc.InitDB,
+	ioc.InitLogger,
+)
+
+func Init() *App {
+	wire.Build(
+		thirdProvider,
+		serviceProviderSet,
+		ioc.InitGRPCxServer,
+		wire.Struct(new(App), "*"),
+	)
+	return new(App)
+}
